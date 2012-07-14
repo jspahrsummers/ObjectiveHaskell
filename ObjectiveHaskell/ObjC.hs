58dffcd59d210e4a6cc16e64c7fd19c89beed7cc
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2012-07-14 01:56:00 -0700

    Adding some more basic Objective-C types and functions to ObjectiveHaskell.ObjC

--- ObjectiveHaskell/ObjC.hs
+++ ObjectiveHaskell/ObjC.hs
@@ -1,15 +1,41 @@
 {-# LANGUAGE ForeignFunctionInterface #-}
 
-module ObjectiveHaskell.ObjC
-    (Sel, Id, p_objc_msgSend)
-    where
+module ObjectiveHaskell.ObjC (
+        Sel, Id, nil,
+        p_objc_msgSend,
+        selector, getClass
+    ) where
 
+import Foreign.C.String
 import Foreign.C.Types
+import Foreign.Marshal.Unsafe
 import Foreign.Ptr
 
+nil :: Id
+nil = nullPtr
+
 type Sel = Ptr ()
+type Class = Ptr ()
+
 -- TODO: this should really be a ForeignPtr with retain/release
 type Id = Ptr ()
 
-foreign import ccall "objc/runtime.h &objc_msgSend"
+foreign import ccall safe "objc/runtime.h &objc_msgSend"
     p_objc_msgSend :: FunPtr (Id -> Sel -> IO Id)
+
+foreign import ccall unsafe "objc/runtime.h sel_registerName"
+    sel_registerName :: CString -> IO Sel
+
+foreign import ccall unsafe "objc/runtime.h objc_getClass"
+    objc_getClass :: CString -> Class
+
+-- Maps a string to a selector
+selector :: String -> IO Sel
+selector s = withCString s sel_registerName
+
+-- Returns the class by the given name
+getClass :: String -> Class
+getClass name =
+    -- objc_getClass() is actually pure, but having to do memory allocation puts us in the IO monad
+    -- Since the stack allocation has no visible effects outside of this function, we unwrap IO
+    unsafeLocalState $ withCString name (return . objc_getClass)
