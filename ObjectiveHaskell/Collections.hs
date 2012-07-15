33d43ff26863b56ae6f2e7184eff8f4a83b268a1
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2012-07-14 20:37:14 -0700

    Adding Haskell functions for Seq -> NSArray and HashTable -> NSDictionary

--- ObjectiveHaskell/Collections.hs
+++ ObjectiveHaskell/Collections.hs
@@ -1,7 +1,8 @@
 {-# LANGUAGE TemplateHaskell #-}
 
 module Collections (
-        fromNSArray, fromNSDictionary
+        fromNSArray, fromNSDictionary,
+        toNSArray, toNSDictionary
     ) where
 
 import Data.Foldable as Foldable
@@ -16,12 +17,24 @@ import Foreign.Marshal.Unsafe
 import ObjectiveHaskell.MsgSend
 import ObjectiveHaskell.ObjC
 
+-- Generic methods
+-- TODO: move these into a module suitable for user importing
+declMessage "objc_copy" "copy" ''Id []
 declMessage "objc_count" "count" ''CSize []
 declMessage "objc_hash" "hash" ''CSize []
 
-declMessage "allKeys" "allKeys" ''Id []
+-- TODO: split NSArray and NSDictionary into two new modules
+
+-- NSArray methods
+declMessage "array" "array" ''Id []
+declMessage "addObject" "addObject:" ''() [''Id]
 declMessage "objectAtIndex" "objectAtIndex:" ''Id [''CSize]
+
+-- NSDictionary methods
+declMessage "allKeys" "allKeys" ''Id []
+declMessage "dictionary" "dictionary" ''Id []
 declMessage "objectForKey" "objectForKey:" ''Id [''Id]
+declMessage "setObjectForKey" "setObject:forKey:" ''() [''Id, ''Id]
 
 -- Converts an NSArray into a Seq.
 fromNSArray :: Id -> IO (Seq Id)
@@ -38,6 +51,14 @@ fromNSArray arr = do
     
     fromNSArray' 0 (return Seq.empty)
 
+-- Converts a Seq into an immutable NSArray.
+toNSArray :: Seq Id -> IO Id
+toNSArray s = do
+    arr <- getClass "NSMutableArray" >>= array
+    mapM (addObject arr) $ Foldable.toList s
+
+    objc_copy arr
+
 -- Hashes an Objective-C object.
 -- This depends on an object's -hash remaining the same (which is required for NSDictionary anyways).
 hash :: Id -> Int32
@@ -50,3 +71,13 @@ fromNSDictionary dict = do
     vals <- mapM (objectForKey dict) keys
 
     HashTable.fromList hash $ List.zip keys vals
+
+-- Converts a HashTable into an immutable NSDictionary.
+toNSDictionary :: HashTable Id Id -> IO Id
+toNSDictionary tbl = do
+    dict <- getClass "NSMutableDictionary" >>= dictionary
+
+    pairs <- HashTable.toList tbl
+    mapM (\(k, v) -> setObjectForKey dict v k) pairs
+
+    objc_copy dict
