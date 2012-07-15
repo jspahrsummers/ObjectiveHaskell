06817ce24ac1977d9f317cf9b47d4cc1a39f95c7
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2012-07-14 18:03:00 -0700

    Split up bridging points in MsgSendTest to better illustrate the idioms for ObjC -> Haskell and Haskell -> ObjC

--- ObjectiveHaskellTests/MsgSendTest.hs
+++ ObjectiveHaskellTests/MsgSendTest.hs
@@ -2,17 +2,22 @@
 
 module MsgSendTest where
 
-import Control.Applicative
+import Control.Monad
 import ObjectiveHaskell.MsgSend
 import ObjectiveHaskell.ObjC
 
 declMessage "stringWithString" "stringWithString:" ''Id [''Id]
 
-msgSendTest unsafeStr = do
-    str <- retainedId unsafeStr
+-- This is what a Haskell function that invokes Objective-C code might look like.
+mutableStringWithString :: Id -> IO Id
+mutableStringWithString str = do
     cl <- getClass "NSMutableString"
-
-    cl `stringWithString` str >>= autorelease
+    cl `stringWithString` str
 
 foreign export ccall
     msgSendTest :: UnsafeId -> IO UnsafeId
+
+-- Bridging points where Objective-C might pass in objects require a bit more work,
+-- to map UnsafeId to Id and back.
+msgSendTest unsafeStr =
+    (mutableStringWithString =<< retainedId unsafeStr) >>= autorelease
