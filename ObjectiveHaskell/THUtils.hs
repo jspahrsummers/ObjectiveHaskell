b7a71ef8e82de0d0d17478be2366aec628085bdf
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2012-07-15 01:48:06 -0700

    Moved argumentNames and singleClauseFunc functions into THUtils

--- ObjectiveHaskell/THUtils.hs
+++ ObjectiveHaskell/THUtils.hs
@@ -1,5 +1,6 @@
 module ObjectiveHaskell.THUtils (
-        decomposeFunctionType
+        decomposeFunctionType,
+        argumentNames, singleClauseFunc
     ) where
 
 import Language.Haskell.TH
@@ -11,3 +12,14 @@ decomposeFunctionType (AppT l (AppT ArrowT r)) = l : decomposeFunctionType r
 decomposeFunctionType (AppT _ t) = decomposeFunctionType t
 decomposeFunctionType (ForallT _ _ t) = decomposeFunctionType t
 decomposeFunctionType t = [t]
+
+-- Generates N unique argument names.
+argumentNames :: Int -> Q [Name]
+argumentNames n = mapM (\s -> newName s) $ take n $ repeat "a"
+
+-- Given a function name, parameter names, and a body expression,
+-- returns a function declaration with a single clause.
+singleClauseFunc :: Name -> [Name] -> Q Exp -> Q Dec
+singleClauseFunc name params bodyExp =
+    let c = clause (map varP params) (normalB bodyExp) []
+    in funD name [c]
