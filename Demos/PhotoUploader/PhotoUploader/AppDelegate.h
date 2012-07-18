1175c4d997bd052101d4ebab504fbf2c6c580a53
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2012-07-17 17:58:30 -0700

    Created PhotoUploader demo project

---
+++ Demos/PhotoUploader/PhotoUploader/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  PhotoUploader
+//
+//  Created by Justin Spahr-Summers on 17.07.12.
+//  Copyright (c) 2012 Justin Spahr-Summers. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ Demos/PhotoUploader/PhotoUploader/AppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.m
+//  PhotoUploader
+//
+//  Created by Justin Spahr-Summers on 17.07.12.
+//  Copyright (c) 2012 Justin Spahr-Summers. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
