94bd23e271cf731b7b17e2592df226efa754f086
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2012-07-19 17:42:12 -0700

    Redefine HsStablePtr as a pointer to an opaque struct (rather than void *), to prevent implicit conversions

--- ObjectiveHaskell/ObjectiveHaskell.h
+++ ObjectiveHaskell/ObjectiveHaskell.h
@@ -11,3 +11,10 @@
 // This makes Haskell functions compatible with ARC, but will effectively
 // prevent ever passing "primitive" C pointers to or from Haskell
 #define HsPtr id
+
+typedef struct OHOpaqueHaskellType *OHOpaqueHaskellPtr;
+
+// This should be compatible with the normal typedef of HsStablePtr to void *,
+// but prevents implicit conversions to other pointer types (because stable
+// pointers shouldn't be inspected in any way)
+#define HsStablePtr OHOpaqueHaskellPtr
