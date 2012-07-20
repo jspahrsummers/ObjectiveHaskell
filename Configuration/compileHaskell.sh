97e4652878585d3c233d736b756d47bbc064f05b
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2012-07-19 17:07:28 -0700

    Use $HEADER_SEARCH_PATHS for GHC imports too

--- Configuration/compileHaskell.sh
+++ Configuration/compileHaskell.sh
@@ -3,4 +3,17 @@
 # Script for compiling *.hs files in an Xcode project, meant to be used with
 # a build rule.
 
-/usr/local/bin/ghc -XForeignFunctionInterface -XTemplateHaskell -framework Foundation -c -O --make "$INPUT_FILE_PATH"
+IMPORTS=""
+
+# Use Objective-C header search paths to locate Haskell modules too
+for DIR in $HEADER_SEARCH_PATHS
+do
+    # Because Haskell module names should be qualified, we need to move one
+    # folder up (which will usually be from the target folder to the project
+    # folder)
+    DIR=`dirname "$DIR"`
+
+    IMPORTS="$IMPORTS -i$DIR"
+done
+
+/usr/local/bin/ghc -XForeignFunctionInterface -XTemplateHaskell -framework Foundation -c -O $IMPORTS --make "$INPUT_FILE_PATH"
