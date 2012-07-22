9942ab4d0def4572662a48fbad58d5d107b4d795
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2012-07-21 17:27:17 -0700

    Change objc_getClass import to be impure, for compatibility with Safe Haskell (it's only ever called from the impure getClass function anyways)

--- ObjectiveHaskell/ObjC.hs
+++ ObjectiveHaskell/ObjC.hs
@@ -114,7 +114,9 @@ selector s = withCString s sel_registerName
 
 -- | Returns the Objective-C class by a given name.
 getClass :: String -> IO Class
-getClass name = withCString name (unretainedId . objc_getClass)
+getClass name =
+    withCString name $ \name ->
+        objc_getClass name >>= unretainedId
 
 -- | Returns the @-hash@ of an object.
 -- | This function assumes that the object's hash will remain the same across calls.
@@ -184,7 +186,7 @@ foreign import ccall safe "objc/runtime.h sel_registerName"
     sel_registerName :: CString -> IO Sel
 
 foreign import ccall safe "objc/runtime.h objc_getClass"
-    objc_getClass :: CString -> UnsafeId
+    objc_getClass :: CString -> IO UnsafeId
 
 foreign import ccall safe "CoreFoundation/CoreFoundation.h CFRetain"
     retain :: UnsafeId -> IO UnsafeId
