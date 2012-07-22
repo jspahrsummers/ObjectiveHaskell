9251ee006b4eb02d9d56c47c614b76e6b8a939bf
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2012-07-21 17:59:46 -0700

    Added a test module for verifying that modules are valid Safe Haskell

---
+++ ObjectiveHaskellTests/SafeHaskellTest.hs
@@ -0,0 +1,12 @@
+{-# LANGUAGE Safe #-}
+
+module SafeHaskellTest where
+
+import ObjectiveHaskell.MsgSend hiding (declMessage)
+import ObjectiveHaskell.NSArray
+import ObjectiveHaskell.NSData
+import ObjectiveHaskell.NSDictionary
+import ObjectiveHaskell.NSObject
+import ObjectiveHaskell.NSString
+import ObjectiveHaskell.NSValue
+import ObjectiveHaskell.ObjC hiding (exportFunc)
