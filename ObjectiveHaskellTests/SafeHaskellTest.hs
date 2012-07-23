d425271854cd039236f4487898bb507201d251c0
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2012-07-23 14:23:38 -0700

    Import NSNumber module in SafeHaskellTest

--- ObjectiveHaskellTests/SafeHaskellTest.hs
+++ ObjectiveHaskellTests/SafeHaskellTest.hs
@@ -6,6 +6,7 @@ import ObjectiveHaskell.MsgSend hiding (declMessage)
 import ObjectiveHaskell.NSArray
 import ObjectiveHaskell.NSData
 import ObjectiveHaskell.NSDictionary
+import ObjectiveHaskell.NSNumber
 import ObjectiveHaskell.NSString
 import ObjectiveHaskell.NSValue
 import ObjectiveHaskell.ObjC hiding (exportFunc)
