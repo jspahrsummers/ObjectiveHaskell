e2d3b0138b574b815f2dba5981fe1bd2bd97961a
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2012-07-29 23:49:09 -0700

    Removed fetching functionality from the User module, replacing it with a decodeUser function (to convert NSData into a User)

--- Demos/PhotoUploader/PhotoUploader/User.hs
+++ Demos/PhotoUploader/PhotoUploader/User.hs
@@ -7,16 +7,11 @@ import Control.Monad
 import Data.Aeson as Aeson
 import Data.ByteString as ByteString
 import Data.Text.Lazy as Text
-import Foreign.Ptr
 import Foreign.StablePtr
 import ObjectiveHaskell.MsgSend
 import ObjectiveHaskell.NSData
 import ObjectiveHaskell.NSString
 import ObjectiveHaskell.ObjC
-
-declMessage "urlWithString" [t| Id -> Class -> IO Id |] "URLWithString:"
-declMessage "requestWithURL" [t| Id -> Class -> IO Id |] "requestWithURL:"
-declMessage "sendSynchronousRequest" [t| Id -> Ptr UnsafeId -> Ptr UnsafeId -> Class -> IO Id |] "sendSynchronousRequest:returningResponse:error:"
 
 type MaybePtr a = StablePtr (Maybe a)
 
@@ -45,20 +40,14 @@ maybeFullName :: MaybePtr User -> IO Id
 maybeFullName ptr =
     deRefStablePtr ptr >>= maybe nil (toNSString . fullName)
 
-fetchCurrentUser :: Id -> IO (MaybePtr User)
-fetchCurrentUser tokenObj = do
-    token <- fromNSString tokenObj
-    urlStr <- toNSString $ Text.append "https://api.instagram.com/v1/users/self?access_token=" token
+decodeUser :: Id -> IO (MaybePtr User)
+decodeUser d = do
+    json <- fromNSData d
 
-    url <- getClass "NSURL" >>= urlWithString urlStr
-    req <- getClass "NSURLRequest" >>= requestWithURL url
-
-    resp <- getClass "NSURLConnection" >>= sendSynchronousRequest req nullPtr nullPtr >>= fromNSData
-
-    let mu = Aeson.decode resp :: Maybe User
+    let mu = Aeson.decode json :: Maybe User
     case mu of
         (Just u) -> newStablePtr $ Just u
         _ -> newStablePtr Nothing
 
-exportFunc "user_fetchCurrent" [t| UnsafeId -> IO (MaybePtr User) |] 'fetchCurrentUser
+exportFunc "user_decode" [t| UnsafeId -> IO (MaybePtr User) |] 'decodeUser
 exportFunc "user_fullName" [t| MaybePtr User -> IO UnsafeId |] 'maybeFullName
