cda1bdf2af89e2112426fc94016c10f98de448e1
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2012-07-29 21:45:05 -0700

    Update Users module to reflect ObjectiveHaskell changes

--- Demos/PhotoUploader/PhotoUploader/Users.hs
+++ Demos/PhotoUploader/PhotoUploader/Users.hs
@@ -5,7 +5,8 @@ module Users where
 import Control.Applicative
 import Control.Monad
 import Data.Aeson as Aeson
-import Data.ByteString.Lazy as ByteString
+import Data.ByteString as ByteString
+import Data.Text.Lazy as Text
 import Foreign.Ptr
 import Foreign.StablePtr
 import ObjectiveHaskell.MsgSend
@@ -21,9 +22,9 @@ type MaybePtr a = StablePtr (Maybe a)
 
 data User = User {
     userId :: Integer,
-    username :: String,
-    fullName :: String,
-    bio :: String
+    username :: Text,
+    fullName :: Text,
+    bio :: Text
 } deriving (Eq, Ord, Show)
 
 instance Aeson.FromJSON User where
@@ -47,14 +48,14 @@ getFullName ptr =
 fetchCurrentUser :: Id -> IO (MaybePtr User)
 fetchCurrentUser tokenObj = do
     token <- fromNSString tokenObj
-    urlStr <- toNSString $ "https://api.instagram.com/v1/users/self?access_token=" ++ token
+    urlStr <- toNSString $ Text.append "https://api.instagram.com/v1/users/self?access_token=" token
 
     url <- getClass "NSURL" >>= urlWithString urlStr
     req <- getClass "NSURLRequest" >>= requestWithURL url
 
     resp <- getClass "NSURLConnection" >>= sendSynchronousRequest req nullPtr nullPtr >>= fromNSData
 
-    let mu = Aeson.decode (fromChunks [resp]) :: Maybe User
+    let mu = Aeson.decode resp :: Maybe User
     case mu of
         (Just u) -> newStablePtr $ Just u
         _ -> newStablePtr Nothing
