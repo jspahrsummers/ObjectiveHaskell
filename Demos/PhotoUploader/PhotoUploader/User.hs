025eb6391b2c842770b2b95f79b5d5b1b95c7f8b
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2012-07-30 01:18:38 -0700

    Refactored the User module to use ObjectiveHaskell.Model

--- Demos/PhotoUploader/PhotoUploader/User.hs
+++ Demos/PhotoUploader/PhotoUploader/User.hs
@@ -8,12 +8,11 @@ import Data.Aeson as Aeson
 import Data.ByteString as ByteString
 import Data.Text.Lazy as Text
 import Foreign.StablePtr
+import ObjectiveHaskell.Model
 import ObjectiveHaskell.MsgSend
 import ObjectiveHaskell.NSData
 import ObjectiveHaskell.NSString
 import ObjectiveHaskell.ObjC
-
-type MaybePtr a = StablePtr (Maybe a)
 
 -- | An Instagram user.
 data User = User {
@@ -39,29 +38,6 @@ instance Aeson.FromJSON User where
     parseJSON j = do
         mzero
 
--- | Dereferences a 'MaybePtr', applies the given accessor to its value,
--- | and then converts the result into an Objective-C object.
-maybeAccessor
-    :: Bridged o
-    => (a -> o)     -- ^ An accessor function to lift into the 'Maybe'.
-    -> MaybePtr a   -- ^ A pointer to dereference.
-    -> IO Id        -- ^ The object result. If the pointer contained 'Nothing', this will be 'nil'.
-
-maybeAccessor f ptr =
-    deRefStablePtr ptr >>= maybe nil (toObjC . f)
-
--- | Returns the full name of a 'User', or 'nil'.
-maybeFullName :: MaybePtr User -> IO Id
-maybeFullName = maybeAccessor fullName
-
--- | Returns the username of a 'User', or 'nil'.
-maybeUsername :: MaybePtr User -> IO Id
-maybeUsername = maybeAccessor username
-
--- | Returns the profile picture URL of a 'User' (as an @NSString@), or 'nil'.
-maybePhotoURL :: MaybePtr User -> IO Id
-maybePhotoURL = maybeAccessor photoURL
-
 -- | Decodes an @NSData@ into a 'User'.
 decodeUser :: Id -> IO (MaybePtr User)
 decodeUser d = do
@@ -72,7 +48,8 @@ decodeUser d = do
         (Just u) -> newStablePtr $ Just u
         _ -> newStablePtr Nothing
 
-exportFunc "user_decode" [t| UnsafeId -> IO (MaybePtr User) |] 'decodeUser
-exportFunc "user_fullName" [t| MaybePtr User -> IO UnsafeId |] 'maybeFullName
-exportFunc "user_username" [t| MaybePtr User -> IO UnsafeId |] 'maybeUsername
-exportFunc "user_photoURL" [t| MaybePtr User -> IO UnsafeId |] 'maybePhotoURL
+exportFunc "User_initWithData" [t| UnsafeId -> IO (MaybePtr User) |] 'decodeUser
+
+exportAccessor "User_fullName" ''User 'fullName
+exportAccessor "User_username" ''User 'username
+exportAccessor "User_photoURL" ''User 'photoURL
