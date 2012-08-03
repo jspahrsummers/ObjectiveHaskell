25defc06ad546d0811350777280d943ffba38985
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2012-08-03 01:12:19 -0700

    Move type declarations/changes from ObjectiveHaskell.h into a new OHTypes header

--- ObjectiveHaskell/ObjectiveHaskell.h
+++ ObjectiveHaskell/ObjectiveHaskell.h
@@ -6,17 +6,5 @@
 //  Released into the public domain.
 //
 
-#import "HsFFI.h"
-
-// This makes Haskell functions compatible with ARC, but will effectively
-// prevent ever passing "primitive" C pointers to or from Haskell
-#define HsPtr id
-
-typedef struct OHOpaqueHaskellType *OHOpaqueHaskellPtr;
-
-// This should be compatible with the normal typedef of HsStablePtr to void *,
-// but prevents implicit conversions to other pointer types (because stable
-// pointers shouldn't be inspected in any way)
-#define HsStablePtr OHOpaqueHaskellPtr
-
 #import "OHModel.h"
+#import "OHTypes.h"
