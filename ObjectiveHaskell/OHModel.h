8a7367101759f207d119d5637966d88796004312
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2012-08-03 01:18:10 -0700

    Starting to make OHModel into a real model class

--- ObjectiveHaskell/OHModel.h
+++ ObjectiveHaskell/OHModel.h
@@ -7,10 +7,44 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "OHTypes.h"
+
+/*
+ * @haskellDynamic(Class, foobar) implements a getter -[Class foobar], which will
+ * call the Haskell function Class_foobar(), passing self.haskellPointer as the
+ * only argument, and return the result.
+ */
+#define haskellDynamic(CLASS, PROPERTY) \
+	dynamic PROPERTY; \
+	\
+	- (id)PROPERTY { \
+		return CLASS ## _ ## PROPERTY(self.haskellPointer); \
+	}
 
 /*
  * A base class for models that are implemented in Haskell using the
  * ObjectiveHaskell.Model module.
  */
 @interface OHModel : NSObject <NSCopying>
+
+/*
+ * Initializes the receiver with the value wrapped by the given Haskell pointer.
+ *
+ * This is the designated initializer for this class.
+ */
+- (id)initWithHaskellPointer:(OHOpaqueHaskellPtr)haskellPointer;
+
+/*
+ * The Haskell pointer to the value wrapped by the receiver.
+ */
+@property (nonatomic, assign, readonly) OHOpaqueHaskellPtr haskellPointer; 
+
+/*
+ * A dictionary representing the properties of the receiver, along with their
+ * associated values.
+ *
+ * This will only include the values associated with @property declarations.
+ */
+@property (nonatomic, copy, readonly) NSDictionary *dictionaryValue;
+
 @end
--- ObjectiveHaskell/OHModel.m
+++ ObjectiveHaskell/OHModel.m
@@ -7,14 +7,59 @@
 //
 
 #import "OHModel.h"
+#import "EXTScope.h"
+#import <objc/runtime.h>
 
 @implementation OHModel
 
 #pragma mark Lifecycle
 
 - (id)init {
-	// TODO
+	NSAssert(NO, @"The designated initializer for OHModel is -initWithHaskellPointer:");
 	return nil;
+}
+
+- (id)initWithHaskellPointer:(OHOpaqueHaskellPtr)haskellPointer {
+	self = [super init];
+	if (self == nil) return nil;
+
+	_haskellPointer = haskellPointer;
+	return self;
+}
+
+#pragma mark Reflection
+
+// The block given here may be invoked more than once for the same property
+// name if that property is implemented on multiple classes in the hierarchy.
++ (void)enumeratePropertiesUsingBlock:(void (^)(objc_property_t property, BOOL *stop))block {
+	Class cls = self;
+
+	// Enumerate all superclasses up to the base class.
+	while (![cls isEqual:[OHModel class]]) {
+		unsigned count = 0;
+		objc_property_t *properties = class_copyPropertyList(cls, &count);
+
+		@onExit {
+			free(properties);
+		};
+
+		for (unsigned i = 0; i < count; i++) {
+			BOOL stop = NO;
+			block(properties[i], &stop);
+
+			if (stop) return;
+		}
+	}
+}
+
+- (NSDictionary *)dictionaryValue {
+	NSMutableSet *propertyNames = [NSMutableSet set];
+
+	[self.class enumeratePropertiesUsingBlock:^(objc_property_t property, BOOL *stop){
+		[propertyNames addObject:@(property_getName(property))];
+	}];
+
+	return [self dictionaryWithValuesForKeys:propertyNames.allObjects];
 }
 
 #pragma mark NSCopying
@@ -26,20 +71,19 @@
 #pragma mark NSObject
 
 - (NSString *)description {
-	// TODO
-	return nil;
+	// TODO: This should use Haskell's Show class instead
+	return [NSString stringWithFormat:@"<%@: %p>%@", self.class, self, self.dictionaryValue];
 }
 
 - (NSUInteger)hash {
-	// TODO
-	return 0;
+	return self.dictionaryValue.hash;
 }
 
 - (BOOL)isEqual:(OHModel *)model {
 	if (![model isMemberOfClass:self.class]) return NO;
 
-	// TODO
-	return YES;
+	// TODO: This should use Haskell's Eq class instead
+	return [self.dictionaryValue isEqualToDictionary:model.dictionaryValue];
 }
 
 @end
