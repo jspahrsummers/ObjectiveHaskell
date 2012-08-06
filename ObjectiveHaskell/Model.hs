3563072d3a5a96b0613fa074f3f6cf2665a64845
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2012-08-05 22:10:22 -0700

    Implemented export of record update functions

--- ObjectiveHaskell/Model.hs
+++ ObjectiveHaskell/Model.hs
@@ -6,6 +6,7 @@ module ObjectiveHaskell.Model (
     ) where
 
 import Control.Monad
+import Data.Char
 import Foreign.StablePtr
 import Language.Haskell.TH
 import ObjectiveHaskell.MsgSend
@@ -16,25 +17,30 @@ import ObjectiveHaskell.THUtils
 -- | This can be used to represent model data that may or may not exist.
 type MaybePtr a = StablePtr (Maybe a)
 
--- | Defines two functions, callable from Objective-C, which get and set a field on a value within a 'MaybePtr'.
+-- | Defines two functions, callable from Objective-C, which get and update a field on a value within a 'MaybePtr'.
 -- |
 -- | For a type @Car@ and a field @model@, the following functions will be generated and exported to Objective-C:
 -- |
 -- |  * A getter @Car_model()@ which accepts a 'MaybePtr' @Car@, reads the @model@ field and converts the result into an
 -- |    Objective-C object. If the 'MaybePtr' contains 'Nothing', @nil@ is returned.
--- |  * A setter @Car_setModel()@ which accepts a 'MaybePtr' @Car@ and a new value for the @model@ field, and returns a
--- |    new 'MaybePtr' @Car@, where the field in the @Car@ value has been set to the given value.
+-- |  * An update function @Car_update_model()@ which accepts a 'MaybePtr' @Car@ and an Objective-C value for the @model@ field, and
+-- |    returns a new 'MaybePtr' @Car@, where the field in the @Car@ value has been updated to the given value (suitably converted
+-- |    to a Haskell type).
 exportAccessors
     :: Name     -- ^ The name of the type of value stored in the 'MaybePtr'.
-    -> Name     -- ^ The name of the record field to access or set in the value, which should hold a 'Bridged' value.
+    -> Name     -- ^ The name of the record field to access in the value, which should hold a 'Bridged' value.
     -> Q [Dec]  -- ^ Top-level declarations to generate and export the accessors.
 
 exportAccessors typeName field = do
-    let getter = (nameBase typeName) ++ "_" ++ (nameBase field)
+    let typeStr = nameBase typeName
+        fieldStr = nameBase field
+
+        getter = typeStr ++ "_" ++ fieldStr
+        setter = typeStr ++ "_update_" ++ fieldStr
+
         t = conT typeName
 
-    -- TODO: export setter
-    exportGetter getter t field
+    liftM2 (++) (exportGetter getter t field) (exportSetter setter t field)
 
 exportGetter
     :: String   -- ^ The name of the function to export.
@@ -52,8 +58,38 @@ exportGetter objcName t field = do
         decl = ForeignD $ ExportF CCall objcName hsName ft
 
     def <- singleClauseFunc hsName [arg] [| do
-                ptr <- deRefStablePtr $(varE arg)
-                obj <- maybe nil (toObjC . $getter) ptr
+                m <- deRefStablePtr $(varE arg)
+                obj <- maybe nil (toObjC . $getter) m
                 autorelease obj |]
+
+    return [decl, def]
+
+-- Internally, we use the term "setter", but we really are talking about record updates (which are not mutations).
+exportSetter
+    :: String   -- ^ The name of the function to export.
+    -> Q Type   -- ^ The type of value stored in the 'MaybePtr'.
+    -> Name     -- ^ The name of the record field to access.
+    -> Q [Dec]
+
+exportSetter objcName t field = do
+    hsName <- newName $ "_hs_" ++ objcName
+    ft <- [t| MaybePtr $t -> UnsafeId -> IO (MaybePtr $t) |]
+
+    let ptrArg = mkName "ptr"
+        valArg = mkName "val"
+
+        decl = ForeignD $ ExportF CCall objcName hsName ft
+
+        -- Record update syntax does not splice well at all.
+        upd = recUpdE (varE $ mkName "v") [return (field, VarE $ mkName "obj")]
+
+    def <- singleClauseFunc hsName [ptrArg, valArg] [| do
+                m <- deRefStablePtr $(varE ptrArg)
+                case m of
+                    (Just v) -> do
+                        obj <- retainedId $(varE valArg) >>= fromObjC
+                        newStablePtr $ Just $upd
+
+                    Nothing -> newStablePtr Nothing |]
 
     return [decl, def]
