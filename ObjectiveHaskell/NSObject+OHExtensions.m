968f6be21eef19ee7edf688a45f0a2c3823cdf9b
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2012-08-08 19:16:25 -0700

    NSObject+OHInitializationExtensions -> NSObject+OHExtensions

--- ObjectiveHaskell/NSObject+OHExtensions.m
+++ ObjectiveHaskell/NSObject+OHExtensions.m
@@ -1,5 +1,5 @@
 //
-//  NSObject+OHInitializationExtensions.m
+//  NSObject+OHExtensions.m
 //  ObjectiveHaskell
 //
 //  Created by Justin Spahr-Summers on 2012-07-13.
@@ -8,14 +8,14 @@
 
 #import <crt_externs.h>
 
-@interface NSObject (OHInitializationExtensions)
+@interface NSObject (OHExtensions)
 @end
 
 /**
  * This private category ensures that the Haskell runtime is loaded at about the
  * same time as the Objective-C runtime.
  */
-@implementation NSObject (OHInitializationExtensions)
+@implementation NSObject (OHExtensions)
 
 + (void)load {
     hs_init(_NSGetArgc(), _NSGetArgv());
